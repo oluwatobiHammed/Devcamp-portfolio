353cb9a6abf7fb1c1c56b118fbbcdf450a391769
oluwatobiHammed <oladipupo_tobi@yahoo.com>
2021-04-24 14:11:24 +0100

    Integrated new gem into rails app for copyright

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,4 +15,8 @@ module ApplicationHelper
       content_tag(:p, greeting, class: "source-greeting")
     end
   end
+
+  def copyright_generator
+    OluwatobiViewTool::Renderer.copyright 'Oluwatobi Hammed', 'All rights reserved'
+  end
 end
