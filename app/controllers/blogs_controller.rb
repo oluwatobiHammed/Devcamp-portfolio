b59437a83c518143f9be6c933c91938d64cc24fd
oluwatobiHammed <oladipupo_tobi@yahoo.com>
2021-05-06 14:14:28 +0100

    Implemented partails for blo, font awesome, and pagination

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -5,7 +5,7 @@
 
           # GET /blogs or /blogs.json
           def index
-            @blogs = Blog.all
+            @blogs = Blog.page(params[:page]).per(5)
             @page_title = "My portfolio blog"
           end
 
